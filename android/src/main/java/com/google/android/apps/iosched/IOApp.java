b289ed21c7348d1596398248018ffa075caf3e81
Nicolas Petitprez <nicolas.petitprez@gmail.com>
2015-06-01 14:05:53 +0200

    configure to report to local server

--- android/src/main/java/com/google/android/apps/iosched/IOApp.java
+++ android/src/main/java/com/google/android/apps/iosched/IOApp.java
@@ -12,6 +12,10 @@ public class IOApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-       
+
+        fr.spoonware.SpoonwareAndroid.init(this,
+                "http://192.168.56.1:8080/rest",
+                "iosched-f3949e81-72ae-4314-b558-eb8fdd42ccc2",  // Application ID
+                "6ntj53qj7bseo1ajp503mfqs76");  // Application TOKEN
     }
 }
